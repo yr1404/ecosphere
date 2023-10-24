f941f0eb31a9f173eaa9795934819d18fd7a527b
yr1404 <yash06765@gmail.com>
2023-10-24 19:58:04 +0530

    fixes database integration error

--- connect.php
+++ connect.php
@@ -5,11 +5,11 @@
     $message = $_POST['message'];
 
     //Database connection 
-    $conn = new mysqli('localhost', 'admin','', 'ecosphere_getInTouch'); 
+    $conn = new mysqli('localhost', 'root','', 'ecosphere_getInTouch'); 
     if($conn->connect_error){ 
         die('Connection Failed : '.$conn->connect_error); 
     }else{ 
-        $stmt = $conn->prepare("insert into registration (name, email, phone, message) 
+        $stmt = $conn->prepare("insert into customer_data (name, email, phone, message) 
         values (?, ?, ?, ?)"); 
         $stmt->bind_param("ssis",$name, $email, $phone, $message); 
         $stmt->execute(); 
