0f13af00f4941d9489f06a04c9f7643e50c8766b
yr1404 <yash06765@gmail.com>
2023-10-25 19:33:03 +0530

    implements automatic whatsapp messages through whatsapp sandbox

--- connect.php
+++ connect.php
@@ -13,8 +13,37 @@
         values (?, ?, ?, ?)"); 
         $stmt->bind_param("ssis",$name, $email, $phone, $message); 
         $stmt->execute(); 
-        echo "registration Successfully..."; 
+        echo "Record added to database Successfully...";
         $stmt->close(); 
         $conn->close();
+    }
+
+    //Whatsapp message to customer
+    require __DIR__ . '/twilio-php-main/src/Twilio/autoload.php'; // Include Twilio PHP SDK
+
+    use Twilio\Rest\Client;
+
+    $accountSid = 'AC97c4672417c53b9a2fbf795ec993d65c';
+    $authToken = '81c9f18ed1643eb51cfd2deb8b6ea68a';
+
+    $client = new Client($accountSid, $authToken);
+
+    $recipientNumber = 'whatsapp:' . '+91'.$phone; // The recipient's WhatsApp number
+    $businessNumber = 'whatsapp:+14155238886'; // Your WhatsApp Business Account number
+
+    $text = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
+
+    try {
+        $message = $client->messages->create(
+            $recipientNumber,
+            array(
+                'from' => $businessNumber,
+                'body' => $text
+            )
+        );
+        //PHP provides nl2br() function to insert the HTML line breaks
+        echo nl2br("\nMessage sent SID: " . $message->sid);
+    } catch (Exception $e) {
+        echo nl2br("\nError: " . $e->getMessage());
     }
 ?>
